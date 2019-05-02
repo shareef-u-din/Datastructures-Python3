ded29e9566db305cd1b534ccfe91f14d266e6bd1
Shareef U Din Bhat <shareef.b@tadigital.com>
2019-05-02 12:14:42 +0530

    add

--- Search/BinarySearch.py
+++ Search/BinarySearch.py
@@ -1,22 +1,20 @@
-def binary_search(l, item):
+def binary_search(list, item):
     """
     :param list: The sorted array as list
     :param item: The element to be found in the list
-    :return: Returns found=1 if element is present and found=0 if not
+    :return: Returns element if found else None
     """
-    size = len(l)
+    size = len(list)
     if size <= 0:
         return None
     start = 0
     end = size - 1
-    found = 0
     while (start <= end):
         mid = int((start + end) / 2)
-        if item == l[mid]:
-            found = 1
-            break
-        if item < l[mid]:
+        if item == list[mid]:
+            return list[mid]
+        if item < list[mid]:
             end = mid - 1
         else:
             start = mid + 1
-    return found
+    return None
--- Search/demo.py
+++ Search/demo.py
@@ -2,4 +2,4 @@ from Search import BinarySearch
 
 
 list=[10,20,30,40,50,60]
-print(" The Return Value : ",BinarySearch.binary_search(list,69))
+print(" The Return Value : ",BinarySearch.binary_search(list,77))
